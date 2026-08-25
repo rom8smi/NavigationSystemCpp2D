952c6702ee30580f9e88198ddb0ab1a95145d05b
rom8smi <rom8smi@gmail.com>
2026-08-25 16:31:58 +0100

    Funnel path (#1)
    * Ported funnel pathfinding from cs
    
    * Tidy up GetPortalPosition method

--- project/src/rts/navigation/pathfinding.hpp
+++ project/src/rts/navigation/pathfinding.hpp
@@ -18,14 +18,19 @@ namespace NavigationSystemCode
         vector<PathfindingNode> nodes;
         vector<Float2> nodePositions;
         vector<vector<int>> nodeNeighbours;
+        vector<int> nodeEdgeRefs;
+        vector<int> nodeEdgeRefsInverted;
         vector<float> additionalCosts;
         vector<bool> additionalCostsModified;
         vector<int> addedAdditionalCosts;
         int nodesCount;
         bool useIterations;
         float costIncrement;
+        bool triangleEdgesMode;
 
         void CreateNodes(NavMesh &navMesh);
+        void CreateNodesCorners(NavMesh &navMesh);
+        void CreateNodesEdges(NavMesh &navMesh);
         Path FindPath(Float2 &startPos, Float2 &targetPos, NavMesh &navMesh);
         Path FindPathWithOrWithoutIterations(Float2 &startPos, Float2 &targetPos, NavMesh &navMesh);
         Path FindPathWithIterations(Float2 &startPos, Float2 &targetPos, NavMesh &navMesh);
@@ -33,8 +38,29 @@ namespace NavigationSystemCode
         Path FindPathToExactTarget(Float2 &startPos, Float2 &targetPos, NavMesh &navMesh);
         void ClearPathSearch();
         void ClearAdditionalCosts();
-        void RetracePath(vector<Float2> &waypoints, int startNode, int endNode, Float2 &startPos, Float2 &targetPos);
-        void SimplifyPath(
+        void RetracePath(
+            vector<Float2> &waypoints,
+            vector<int> &waypointIndices,
+            int startNode,
+            int endNode);
+        void SimplifyPathEdges(
+            vector<Float2> &waypoints,
+            vector<int> &waypointIndices,
+            NavMesh &navMesh);
+        void SimplifyPathEdgesInner(
+            vector<Float2> &waypoints,
+            vector<int> &leftPortalsEdgeIndices,
+            vector<int> &rightPortalsEdgeIndices,
+            NavMesh &navMesh,
+            vector<Float2> &simplifiedWaypoints);
+        void GetPortalPosition(
+            int index,
+            int cornerIndex,
+            vector<Float2> &waypoints,
+            NavMesh &navMesh,
+            Float2 &position);
+        float Orient2D(Float2 &a, Float2 &b, Float2 &c);
+        void SimplifyPathCorners(
             vector<Float2> &waypoints,
             NavMesh &navMesh);
         bool CanWaypointsBeMerged(
@@ -42,7 +68,8 @@ namespace NavigationSystemCode
             int i,
             NavMesh &navMesh);
         vector<Float2> ReversePath(vector<Float2> &waypoints);
-        int UpdatePositionNode(Float2 &position, NavMesh &navMesh, int nodeIndex);
+        int UpdatePositionNodeEdges(Float2 &position, NavMesh &navMesh, int nodeIndex);
+        int UpdatePositionNodeCorners(Float2 &position, NavMesh &navMesh, int nodeIndex);
         float GetDistance(int nodeA, int nodeB);
     };
 }
